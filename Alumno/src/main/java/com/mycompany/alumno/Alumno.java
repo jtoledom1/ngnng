c25f07fa236c66c04f51b4176c5b81d1afba6fd2
jtoledom1 <100723562+jtoledom1@users.noreply.github.com>
2023-10-29 04:47:24 -0600

    Add files via upload

---
+++ Alumno/src/main/java/com/mycompany/alumno/Alumno.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alumno;
+
+/**
+ *
+ * @author Jtole
+ */
+public class Alumno {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
